73c8afdd5d1aa7c901db2e8c3901ce16df133b9a
Sam Desborough <sam.desborough@gmail.com>
2014-11-27 12:42:31 +0000

    initial commit

---
+++ reload.js
@@ -0,0 +1,41 @@
+(function () {
+  'use strict';
+
+  var fs = require('fs'),
+      http = require('http'),
+      express = require('express'),
+
+      app = express(),
+      server = http.createServer(app),
+      io = require('socket.io')(server),
+
+      filename = __dirname + '/test',
+      prevChange = -Infinity;
+
+  function now () {
+    return process.hrtime()[0];
+  }
+
+  fs.watch(filename, function (event, filename) {
+    var currChange = now();
+
+    /**
+     * ensure 3 seconds between each change
+     */
+    if (currChange - prevChange > 3) {
+      io.emit('file::change', currChange);
+      prevChange = currChange;
+    }
+  });
+
+  app.use(express.static(__dirname + '/static'));
+
+  app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/static/index.html');
+  });
+
+  server.listen(8080, function () {
+    var address = server.address();
+    console.log('running on http://127.0.0.1:%j', address.port);
+  });
+})();
---
+++ static/client.js
@@ -0,0 +1,41 @@
+(function () {
+  'use strict';
+
+  var socket = io.connect('http://localhost:8080'),
+      preview = document.getElementById('preview'),
+
+      name = 'VandA',
+      format = 'woff',
+      baseUrl = '/VandA.' + format;
+
+  function generate (family, url) {
+    return "\n" +
+      "@font-face {\n" +
+        "  font-family: '" + family + "';\n" +
+        "  src: url('" + url + "') format(" + format + ");\n" +
+      "}\n";
+  }
+
+  function createStyle (id) {
+    var family = [name, id].join('_'),
+        url = [baseUrl, 'v=' + id].join('?'),
+        oldStyle = document.getElementsByTagName('style')[0],
+        style = document.createElement('style');
+
+    style.appendChild(document.createTextNode(generate(family, url)));
+
+    document.head.appendChild(style);
+    document.head.removeChild(oldStyle);
+
+    return family;
+  }
+
+  socket.on('file::change', function (data) {
+    var family = createStyle(data);
+    preview.style['font-family'] = family;
+  });
+
+  window.onload = function () {
+    preview.focus();
+  };
+})();
